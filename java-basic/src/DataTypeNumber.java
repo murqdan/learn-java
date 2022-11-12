eecd936035e4a24f31cd7c6a2a2c1db56bbdcdbc
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-13 00:59:56 +0700

    learn data type number

---
+++ java-basic/src/DataTypeNumber.java
@@ -0,0 +1,23 @@
+public class DataTypeNumber {
+    public static void main(String[] args) {
+        // Integer Number
+        byte thisIsByte = 100;
+        short thisIsShort = 1000;
+        int thisIsInt = 10000000;
+        long thisIsLong = 100000000;
+        long thisIsLong2 = 100000000L;
+
+        // Floating Point Number
+        float thisIsFloat = 10.10F;
+        double thisIsDouble = 10.10;
+
+        // Literals Number
+        int decimalInt = 34;
+        int hexaDecimal = 0xFFFFFF;
+        int binaryDecimal = 0b10101010;
+
+        // Underscore
+        int amount = 1_000_000_000;
+        int sum = 70_000_000;
+    }
+}
