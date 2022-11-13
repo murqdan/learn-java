4bd3a24e65a66fd8d641b837b175bd19d6617771
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-13 10:29:52 +0700

    learn data type string

---
+++ java-basic/src/DataTypeString.java
@@ -0,0 +1,11 @@
+public class DataTypeString {
+    public static void main(String[] args) {
+        String firstName = "Muhammad Rafiqul";
+        String lastName = "Adnan";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
