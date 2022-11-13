9d0a4473088bf45092a6e18086b1f33b102696ec
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-13 10:12:36 +0700

    learn data type char

---
+++ java-basic/src/DataTypeCharacter.java
@@ -0,0 +1,13 @@
+public class DataTypeCharacter {
+    public static void main(String[] args) {
+        char l = 'L';
+        char o = 'O';
+        char v = 'V';
+        char e = 'E';
+
+        System.out.print(l);
+        System.out.print(o);
+        System.out.print(v);
+        System.out.print(e);
+    }
+}
