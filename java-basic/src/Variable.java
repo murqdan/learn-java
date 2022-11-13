d930b959c4238e52938da25bade5f55aa0738d23
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-13 10:54:28 +0700

    learn variable

---
+++ java-basic/src/Variable.java
@@ -0,0 +1,26 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Muhammad Rafiqul Adnan";
+
+        int age = 22;
+
+        String address = "Yogyakarta";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Uvuvwevwevwe Onyetenyevwe Ugwemuhwem Osas";
+        System.out.println(name);
+
+        var fruit = "Mango";
+        var sum = 100;
+
+        System.out.println(fruit);
+        System.out.println(sum);
+
+        final var quote = "Now or Never";
+        System.out.println(quote);
+    }
+}
