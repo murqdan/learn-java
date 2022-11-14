9d4c68d0fca8cb31a91deb38548c2f85fc0682b1
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-14 10:02:35 +0700

    learn expression, statement, and block

---
+++ java-basic/src/Block.java
@@ -0,0 +1,15 @@
+public class Block {
+    public static void main(String[] args) {
+        int value;  // expression
+        value = 10; // expression
+
+        double aValue = 8933.234; // assignment statement
+        aValue++; // increment statement
+        System.out.println("Hello World 1"); // method invocation statement
+        Date date = new Date(); // object creation statement
+
+        {
+            System.out.println("Hello World 2");
+        }
+    }
+}
