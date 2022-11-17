d8888cd4cff402a9b1f891bdfa996e8ef99d7627
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-17 11:01:53 +0700

    make view removeTodoList

--- todolist-app/src/TodoListApp.java
+++ todolist-app/src/TodoListApp.java
@@ -167,7 +167,18 @@ public class TodoListApp {
     }
 
     public static void viewRemoveTodoList() {
+        System.out.println("MENGHAPUS TODOLIST");
 
+        var number = input("Nomor yang dihapus (x Jika Batal)");
+
+        if (number.equals("x")) {
+            // batal
+        } else {
+            boolean success = removeTodoList(Integer.valueOf(number));
+            if (!success) {
+                System.out.println("Gagal menghapus todolist nomor : " + number);
+            }
+        }
     }
 
 
