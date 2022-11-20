33c03cb6c8154d54fe105e4dbee982f071fb061d
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-20 12:44:26 +0700

    make constructor

--- java-oop/src/PersonApp.java
+++ java-oop/src/PersonApp.java
@@ -1,16 +1,16 @@
 public class PersonApp {
     public static void main(String[] args) {
-        var person1 = new Person();
-        person1.name = "Rafiq";
-        person1.address = "Yogyakarta";
+        var person1 = new Person("Rafiq", "Yogyakarta");
 
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.country);
-        person1.sayHello("Osas");
 
-        Person person2 = new Person();
+        person1.sayHello("Budi");
+
+        Person person2 = new Person("Joko", "Sleman");
+
         Person person3;
-        person3 = new Person();
+        person3 = new Person("Isa", "Sleman");
     }
 }
