d85e488be957783de666b6901e53897c4d7fb5fc
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-21 23:55:46 +0700

    type check and casts

--- java-oop/src/PolymorphismApp.java
+++ java-oop/src/PolymorphismApp.java
@@ -15,6 +15,17 @@ public class PolymorphismApp {
     }
 
     static void introduce(Employee employee) {
+        if (employee instanceof VicePresident) {
+            VicePresident vicePresident = (VicePresident) employee;
+            System.out.println("Hello VP " + vicePresident.name);
+        } else if (employee instanceof Manager) {
+            Manager manager = (Manager) employee;
+            System.out.println("Hello Manager " + manager.name);
+        } else {
+            System.out.println("Hello " + employee.name);
+        }
+
         System.out.println("I'm " + employee.name);
+
     }
 }
