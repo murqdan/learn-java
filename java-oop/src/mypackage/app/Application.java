80194364acd73b7908d2408f95a4124bec9abe53
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-24 22:35:33 +0700

    import *

--- java-oop/src/mypackage/app/Application.java
+++ java-oop/src/mypackage/app/Application.java
@@ -1,11 +1,13 @@
 package mypackage.app;
 
-import mypackage.data.Product;
+import mypackage.data.*;
 
 public class Application {
     public static void main(String[] args) {
         Product product = new Product("Mac Book Pro", 30_000_000);
         System.out.println(product.name);
         System.out.println(product.price);
+
+        Data data = new Data();
     }
 }
