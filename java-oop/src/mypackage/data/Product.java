dbf3bf0b7e3ccf9ecd1295c1b7cea72bb9a3b3ef
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-24 13:22:23 +0700

    package

---
+++ java-oop/src/mypackage/data/Product.java
@@ -0,0 +1,11 @@
+package mypackage.data;
+
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
