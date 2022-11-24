a808fe9351ebcd1ecca15e4ede5eaddf3dce6571
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-24 14:04:49 +0700

    access modifier

---
+++ java-oop/src/mypackage/app/Application.java
@@ -0,0 +1,11 @@
+package mypackage.app;
+
+import mypackage.data.Product;
+
+public class Application {
+    public static void main(String[] args) {
+        Product product = new Product("Mac Book Pro", 30_000_000);
+        System.out.println(product.name);
+        System.out.println(product.price);
+    }
+}
