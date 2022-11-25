19ca433ddc9adf5a9bd20f72a63c24bd154492d1
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-25 20:28:20 +0700

    override toString() method

--- java-oop/src/mypackage/app/ProductApp.java
+++ java-oop/src/mypackage/app/ProductApp.java
@@ -2,11 +2,12 @@ package mypackage.app;
 
 import mypackage.data.*;
 
-public class Application {
+public class ProductApp {
     public static void main(String[] args) {
         Product product = new Product("Mac Book Pro", 30_000_000);
         System.out.println(product.name);
         System.out.println(product.price);
+        System.out.println(product);
 
         Data data = new Data();
     }
