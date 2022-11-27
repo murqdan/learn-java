b5a9c244e5e39e86eb56a1b618231773da2990b4
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-27 21:35:01 +0700

    static keyword

--- java-oop/src/mypackage/util/MathUtil.java
+++ java-oop/src/mypackage/util/MathUtil.java
@@ -1,2 +1,11 @@
-package mypackage.util;public class MathUtil {
+package mypackage.util;
+
+public class MathUtil {
+    public static int sum(int... values) {
+        int total = 0;
+        for (var value : values) {
+            total += value;
+        }
+        return total;
+    }
 }
