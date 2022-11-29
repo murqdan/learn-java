9866d7bc84e3c1dd54c4ceb2ed1439f492a4dfe5
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-29 15:29:27 +0700

    record class constructor

--- java-oop/src/mypackage/app/RecordApp.java
+++ java-oop/src/mypackage/app/RecordApp.java
@@ -9,5 +9,9 @@ public class RecordApp {
         System.out.println(loginRequest.username());
         System.out.println(loginRequest.password());
         System.out.println(loginRequest);
+
+        System.out.println(new LoginRequest());
+        System.out.println(new LoginRequest("eko"));
+        System.out.println(new LoginRequest("eko", "rahasia"));
     }
 }
--- java-oop/src/mypackage/data/LoginRequest.java
+++ java-oop/src/mypackage/data/LoginRequest.java
@@ -1,5 +1,19 @@
 package mypackage.data;
 
 public record LoginRequest(String username, String password) {
+    public LoginRequest{
+        System.out.println("make object LoginRequest");
+    }
 
+    public LoginRequest(String username) {
+        this(username, "");
+    }
+
+    public LoginRequest() {
+        this("", "");
+    }
+
+    public void sayHello() {
+        System.out.println("Hello " + this.username);
+    }
 }
