354e59718334f560f15f3a80a9e715976ffae2b5
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-11-30 19:23:11 +0700

    annotation

--- java-oop/src/mypackage/app/AnimalApp.java
+++ java-oop/src/mypackage/app/AnimalApp.java
@@ -1,8 +1,10 @@
 package mypackage.app;
 
+import mypackage.annotation.Fancy;
 import mypackage.data.Animal;
 import mypackage.data.Cat;
 
+@Fancy(name = "AnimalApp", tags = {"application", "java"})
 public class AnimalApp {
     public static void main(String[] args) {
         Animal animal = new Cat();
